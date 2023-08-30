9dc81861e9ad2399d71f4bd0def6fecff7e49ffc
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-31 01:56:37 +1000

    added login function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/models/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +14,15 @@ import { FormsModule } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  ngOnInit(): void {
+  constructor(private AuthService: AuthService, 
+    private router: Router) { }
+
+  email:string = "";
+  password:string = "";
+  errormsg: string ="";
+  newuser:User = new User();
+  loggedin:boolean = false;
+  ngOnInit() {
     if (sessionStorage.getItem('currentUser')){
       this.loggedin = true;
     }else{
@@ -19,12 +30,29 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  loggedin:boolean = false;
-  email:string = "";
-  pwd:string = "";
-
   signin(event:any){
-
+    console.log("at signin");
+    event.preventDefault();
+    this.AuthService.login(this.email,this.password).subscribe({
+      next:
+        (data)=>{
+          if (data.valid == true){
+            this.newuser = new User(data.username, data.email,data.id, data.password,
+               data.roles, data.group, data.valid)
+            this.AuthService.setCurrentuser(this.newuser);
+            this.router.navigate(['/profile']);
+          }else{
+           
+            this.errormsg = "There is a problem with the credentials";
+          }
+      
+      error:
+        this.errormsg = "There is a problem with the credentials";
+      
+    }
+      
+  })
   }
 
 }
+
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/models/user';
+import{ HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+
+  isLoggedin(){
+    if (sessionStorage.getItem('currentUser')){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  login(email:string,password:string){
+    return this.http.post<User>('http://localhost:3000/api/auth', { email: email , password: password});
+  }
+
+  setCurrentuser(newuser:User){
+    sessionStorage.setItem('currentUser',JSON.stringify(newuser));
+  }
+  getCurrentuser(){
+    return sessionStorage.getItem('currentUser');
+  }
+
+  logout(event:any){
+    sessionStorage.removeItem('currentUser');
+    this.router.navigateByUrl('/login');
+
+  }
+
 }
