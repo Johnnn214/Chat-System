5e1bb4b1d4a19605d9fefe812a5e79283d7a3b53
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-30 22:46:28 +1000

    added login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  ngOnInit(): void {
+    if (sessionStorage.getItem('currentUser')){
+      this.loggedin = true;
+    }else{
+      this.loggedin = false;
+    }
+  }
+
+  loggedin:boolean = false;
+  email:string = "";
+  pwd:string = "";
+
+  signin(event:any){
+
+  }
 
 }
