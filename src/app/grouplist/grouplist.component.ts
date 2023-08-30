d40854b30b8f8b078129e3c7bb0e5bfe201069b3
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-30 22:46:04 +1000

    added user list and grouplist component

---
+++ src/app/grouplist/grouplist.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-grouplist',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './grouplist.component.html',
+  styleUrls: ['./grouplist.component.css']
+})
+export class GrouplistComponent {
+
+}
---
+++ src/models/userlist.ts
@@ -0,0 +1,11 @@
+export class Userlist {
+
+    list: Array<string>;
+
+    constructor(list:Array<string>=[]){
+       
+        this.list = list;
+    
+    }
+    
+}
