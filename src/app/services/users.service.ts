70a3a1658da564b173a32633a879ced625663f66
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-31 06:42:54 +1000

    created users.service for returning usersnames

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from 'src/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) { }
+
+
+  getAllUsernames(roles:string){
+    return this.http.post<any>('http://localhost:3000/api/getusers',{roles: roles});
+  }
+
+}
