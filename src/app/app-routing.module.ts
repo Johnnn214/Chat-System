eb31d8480d27be0e866fea4527c0e4d3a60e6cb7
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-30 18:30:38 +1000

    added routes for all pages and nav bar in app.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { GroupComponent } from './group/group.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { RolesComponent } from './roles/roles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chat', component: ChatComponent},
+  { path: 'group',component: GroupComponent},
+  { path: 'login',component: LoginComponent},
+  { path: 'profile',component: ProfileComponent},
+  { path: 'roles',component: RolesComponent},
+  { path: 'signup',component: SignupComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
