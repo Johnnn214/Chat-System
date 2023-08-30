5aaf4fd69a4899637b2fc560098f0c237f24365c
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-30 18:13:26 +1000

    created chat , group login, profile, roles, signup component

---
+++ src/app/group/group.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-group',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './group.component.html',
+  styleUrls: ['./group.component.css']
+})
+export class GroupComponent {
+
+}
