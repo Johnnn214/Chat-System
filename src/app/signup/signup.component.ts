081a2588bf5522ef6c142d81d1a80f4e8fdbd33b
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-30 22:47:19 +1000

    added signup form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit  {
+
+  ngOnInit(): void {
+
+  }
+
+  username:string ="";
+  email:string ="";
+  pwd:string ="";
+  
+  signup(event:any){
+    
+  }
+  
 
 
 }
