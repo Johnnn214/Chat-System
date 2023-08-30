230049d43cc4cd8f637481cfae7a0372e1c6aa4d
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-30 23:43:37 +1000

    added server route login and user.json

---
+++ server/routes/login.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+
+module.exports = function (req, res) {
+        if (!req.body) {
+            return res.sendStatus(400)
+        }
+        fs.readFile('../server/data/user.json','utf8',(err,data)=>{
+            if (err) {
+                console.error(err)
+                return
+            }
+
+            try{
+            console.log(data);
+            let users = JSON.parse(data);
+            users = users.users;
+            console.log(users);
+              
+            var customer = {};
+    
+            customer.valid = false;
+            customer.email = '';
+            customer.username = '';
+            customer.roles = [];
+            customer.group = [];
+    
+            for (let i = 0; i < users.length; i++) {
+                if (req.body.email == users[i].email && req.body.password == users[i].password) {
+                    customer.valid = true;
+                    customer.email = users[i].email;
+                    customer.username = users[i].username;
+                    customer.roles = users[i].roles;
+                    customer.group = users[i].group;
+                }
+            }
+            res.send(customer);
+            
+            }catch(err){
+              console.log("Error pasing the userdata");
+            }
+           })
+    
+
+}
---
+++ server/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+var fs = require('fs');
+var cors = require('cors')
+app.use(cors());
+app.use (express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()) ;
+
+//app.use(express.static(__dirname, '../dist/'));
+
+
+
+let server = http.listen( 3000, function () {
+let host = server.address().address;
+let port = server.address().port;
+console.log("Server listening on: "+ host + " port: " + port);
+});
+
+app.post('/api/auth' ,require('./routes/login'));
