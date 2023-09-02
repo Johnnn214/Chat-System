30aefb74b281683972569c62cec7c3e89294dd2b
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-09-03 04:34:51 +1000

    added funtion only superadmin can view roles pages

--- src/app/pages/roles/roles.component.ts
+++ src/app/pages/roles/roles.component.ts
@@ -14,6 +14,28 @@ import { SuperlistComponent } from 'src/app/users/superlist/superlist.component'
 })
 export class RolesComponent {
   
+  isadmin:boolean = false;
+  user:any | null = sessionStorage.getItem('currentUser');
+  admin:string = "super"
+  user1 = JSON.parse(this.user);
+  role:Array<string> | null | undefined;
+
+  
+  ngOnInit() {
+    if (this.role == null){
+      console.log("role is empty")
+    }
+    else {
+      this.role == this.user1.roles;
+      if (this.role?.includes(this.admin)){
+      this.isadmin = true;
+      }else{
+        this.isadmin = false;
+      }
+    }
+    console.log(this.isadmin);
+    console.log(this.role);
+  }
 
 
 }
