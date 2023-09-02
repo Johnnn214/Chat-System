90b205ba2dcac59ff9734e02ff32c6664f407442
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-09-03 05:14:10 +1000

    created footer and updated nav

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,5 +10,37 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+    
+  constructor(private authServices: AuthService) { }
+
+  isadmin:boolean = false;
+  issuperadmin: boolean= false;
+  user:any = sessionStorage.getItem('currentUser');
+  superadmin:string = "super"
+  user1 = JSON.parse(this.user);
+  role!: Array<string>;
+
+  ngOnInit() {
+    if (this.user1 != null){
+      this.role = this.user1.roles;
+      if (this.role.includes(this.superadmin)){
+      this.issuperadmin = true;
+      }else{
+        this.issuperadmin = false;
+      }
+      console.log("user ",this.user1);
+      console.log("roles", this.role);
+      console.log(this.issuperadmin);
+    }
+    else {
+      console.log("role is empty")
+
+    }
+
+  }
+
+  logout(event:any){
+    this.authServices.logout(event);
+    }
 
 }
