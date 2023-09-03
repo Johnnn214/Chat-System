57b27b5f99a3c84ccf5f6a43e90807325465f54d
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-09-03 17:27:48 +1000

    created channel component

---
+++ src/app/channel/channel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-channel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './channel.component.html',
+  styleUrls: ['./channel.component.css']
+})
+export class ChannelComponent {
+
+}
