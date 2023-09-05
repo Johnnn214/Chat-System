83dddb29b59ed33a42952d1c9219570fc324a1c2
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-09-06 04:11:55 +1000

    updated group model

--- src/app/models/group.ts
+++ src/app/models/group.ts
@@ -3,14 +3,16 @@ import { Channel } from '../models/channel';
 export class Group {
     name:string;
     id:string;
-    channel: Array<Channel>;
-  admin: any;
+    channel: Array<string>;
+    admin:Array<string>
 
-    constructor(name:string='', id:string="",channel:Array<Channel>=[]){
+
+    constructor(name:string='', id:string="",channel:Array<string>=[], admin:Array<string>=[]){
        
         this.name = name;
         this.id = id;
         this.channel = channel;
+        this.admin =admin;
     
     }
     
